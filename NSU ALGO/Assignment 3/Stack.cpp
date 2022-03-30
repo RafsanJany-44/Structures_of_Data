accda582e43d289f64e8b6b736b2069ac1c2f4e0
Rafsan Jany <59754354+RafsanJany-44@users.noreply.github.com>
2022-03-30 20:39:39 +0600

    update

---
+++ NSU ALGO/Assignment 3/Stack.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+struct Item{
+    int data;
+    Node *next;
+};
+
+class Stack{
+private:
+    Node *top;
+public:
+    Stack(){
+        top = NULL;
+    }
+
+    void push(int data){
+
+    }
+
+    void pop(){
+
+    }
+
+    int top(){
+
+    }
+
+    bool isEmpty(){
+
+    }
+
+    void makeEmpty(){
+
+    }
+};
+
+int main(){
+    //Complete the functions above
+    //Call each function here in the main()
+    //to test if they are working correctly
+}
