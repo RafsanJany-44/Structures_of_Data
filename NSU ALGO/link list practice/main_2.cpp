94e74d4d573062fb7ae6c30011544e569e5433c1
Rafsan Jany <59754354+RafsanJany-44@users.noreply.github.com>
2022-04-07 00:36:36 +0600

    up

---
+++ NSU ALGO/link list practice/main_2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node *next;
+};
+
+class LinkedList{
+private:
+    Node *head;
+
+public:
+
+    LinkedList()
+    {
+        head= NULL;
+    }
+
+    void addNodeAtEnd(int data){
+        
+    }
+    
+
+
+
+};
