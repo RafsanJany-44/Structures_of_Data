6b5c7e3bd107b92bbd6bb70ec9005c78d46b4733
Rafsan Jany <59754354+RafsanJany-44@users.noreply.github.com>
2022-04-08 11:19:10 +0600

    up

--- Sector_2/LinkedList_ALL_FUN/main.cpp
+++ Sector_2/LinkedList_ALL_FUN/main.cpp
@@ -192,11 +192,12 @@ int main(){
     cout<<"Showing the ith index: ";
     cout<<l.get(3);
     l.printAllNode();*/
-    cout<<l.get(-2);
-    cout<<l.len();
-
+    cout<<l.get(5);
+   // cout<<l.len();
+    cout<<endl;
     for(int i=0;i<l.len();i++){
-        cout<<l.get(i)<<endl;
+        cout<<i<<","<<l.get(i)<<endl;
+
     }
     
     
